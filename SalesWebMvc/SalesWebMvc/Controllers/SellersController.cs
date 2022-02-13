aa9d0626b4d33d55565469038aa60b3b6b4005cc
Rodolfo Leal Braga <75742317+rodox021@users.noreply.github.com>
2022-02-13 13:29:38 -0300

    Detalhes com eager load - Seller -> department

--- SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -52,7 +52,7 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
-
+        //-----------------------------------------------------------------
         public IActionResult Delete (int? id)
         {
             if (id == null)
@@ -73,6 +73,20 @@ namespace SalesWebMvc.Controllers
         {
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
+        }
+        //-----------------------------------------------------------------
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
         }
     }
 }
