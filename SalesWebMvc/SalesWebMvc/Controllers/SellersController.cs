68f78b903b1f08c545c96c79fb6af7ebd2b3e602
Rodolfo Leal Braga <75742317+rodox021@users.noreply.github.com>
2022-02-13 12:19:41 -0300

    Metodo delete Seller

--- SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -21,7 +21,7 @@ namespace SalesWebMvc.Controllers
             _sellerService = sellerService;
             _departmentService = departmentService;
         }
-
+        //-----------------------------------------------------------------
         public IActionResult Index()
         {
             var department = _departmentService.FindAll();
@@ -37,17 +37,41 @@ namespace SalesWebMvc.Controllers
 
             return View(l);
         }
+        //-----------------------------------------------------------------
         public IActionResult Create()
         {
             var department = _departmentService.FindAll();
             var viewModel = new SellerFormViewModel { Departments = department };
             return View(viewModel);
         }
+        //-----------------------------------------------------------------
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete (int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //-----------------------------------------------------------------
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
