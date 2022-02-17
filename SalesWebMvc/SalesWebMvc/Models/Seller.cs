9cc2ddb5f32bbcc42bd4145ba011e144fc9432b6
Rodolfo Leal Braga <75742317+rodox021@users.noreply.github.com>
2022-02-16 23:18:53 -0300

    Criando os data notation nos atributos da classe Seller

--- SalesWebMvc/SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,28 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0: dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Salário Base")]
+       [DataType(DataType.Currency)] // mostra o campo com o valor formatado para sua região que configurado no startuo, R$ 1000,00
+       //[DisplayFormat(DataFormatString = "{0:F2}")] // vai mostrar o campo com o valor separado com 2 casa decimais 1000,00
         public double BaseSalary { get; set; }
+
+        [Display(Name = "Departamento")]
         public Department Department { get; set; }
+
+        [Display(Name = "Departamento")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
